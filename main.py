e6002b6a77109320b166607b1f145ef00af6f872
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-08-03 13:34:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,106 @@
-allUserstoCheck = [
-    {"name": "icebergy_"},
-    {"name": "Pentosh1"},
-    {"name": "ASvanevik"},
-]
-
-
-import requests, json, os
+import requests, json, os, tweepy
 from dotenv import load_dotenv
+from http.server import BaseHTTPRequestHandler
 
 load_dotenv()
 
 
-def getUserID(userName):
-    url = "https://api.twitter.com/2/users/by/username/" + str(userName)
-    payload = {}
-    headers = {"Authorization": os.getenv("Bear")}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    json_obj = json.loads(response.text)
-    Id = json_obj["data"]["id"]
-    return Id
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        auth = tweepy.OAuthHandler(
+            os.getenv("consumer_key"), os.getenv("consumer_secret")
+        )
+        auth.set_access_token(os.getenv("access_token"), os.getenv("access_secret"))
+        api = tweepy.API(auth)
+        msg = self.twit(
+            [
+                {"name": "iamDCinvestor"},
+                {"name": "j1mmyeth"},
+                {"name": "DCLBlogger"},
+                {"name": "Zeneca_33"},
+                {"name": "NFTsAreNice"},
+                {"name": "VonMises14"},
+                {"name": "gmoneyNFT"},
+            ]
+        )
+        api.update_status(status=msg)
+        self.wfile.write(str(msg).encode())
+        return
 
+    def getUserID(self, userName, allUserstoCheck):
+        url = "https://api.twitter.com/2/users/by/username/" + str(userName)
+        payload = {}
+        headers = {"Authorization": os.getenv("Bear")}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        json_obj = json.loads(response.text)
+        Id = json_obj["data"]["id"]
+        return Id
 
-def getFollowingsByUserName(userName):
-    userid = getUserID(userName)
-    names = []
+    def getFollowingsByUserName(self, userName, allUserstoCheck):
+        userid = self.getUserID(userName, allUserstoCheck)
+        names = []
 
-    NextToken = ""
-    resp_json = getFollowings(userid)
-    for user in resp_json["data"]:
-        names.append(user["name"])
-    return names
+        NextToken = ""
+        resp_json = self.getFollowings(userid, allUserstoCheck)
+        for user in resp_json["data"]:
+            names.append(user["name"])
+        return names
 
+    def getFollowings(self, userID, allUserstoCheck):
+        url = (
+            "https://api.twitter.com/2/users/"
+            + str(userID)
+            + "/following"
+            + "?max_results=50"
+        )
+        payload = {}
+        headers = {"Authorization": os.getenv("Bear")}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        json_obj = json.loads(response.text)
+        return json_obj
 
-def getFollowings(userID):
-    url = (
-        "https://api.twitter.com/2/users/"
-        + str(userID)
-        + "/following"
-        + "?max_results=10"
-    )
-    payload = {}
-    headers = {"Authorization": os.getenv("Bear")}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    json_obj = json.loads(response.text)
-    return json_obj
+    def getIntersect(self, A, B, C, allUserstoCheck):
+        AFoll = A["followings"]
+        BFoll = B["followings"]
+        for a in AFoll:
+            if a in BFoll:
+                if a not in C:
+                    C[a] = []
+                C[a].append(A["name"])
+                C[a].append(B["name"])
+                C[a] = list(set(C[a]))
 
+    def twit(self, allUserstoCheck):
+        for i in range(len(allUserstoCheck)):
+            allUserstoCheck[i]["followings"] = self.getFollowingsByUserName(
+                allUserstoCheck[i]["name"], allUserstoCheck
+            )
+        intersect = {}
+        for i in range(len(allUserstoCheck)):
+            for j in range(i, len(allUserstoCheck)):
+                self.getIntersect(
+                    allUserstoCheck[i], allUserstoCheck[j], intersect, allUserstoCheck
+                )
+        tweet = "Trending Follows:\n\n"
+        index = 1
 
-for i in range(len(allUserstoCheck)):
-    allUserstoCheck[i]["followings"] = getFollowingsByUserName(
-        allUserstoCheck[i]["name"]
-    )
-
-
-def getIntersect(A, B, C):
-    AFoll = A["followings"]
-    BFoll = B["followings"]
-    for a in AFoll:
-        if a in BFoll:
-            if a not in C:
-                C[a] = []
-            C[a].append(A["name"])
-            C[a].append(B["name"])
-            C[a] = list(set(C[a]))
-
-
-intersect = {}
-for i in range(len(allUserstoCheck)):
-    for j in range(i, len(allUserstoCheck)):
-        getIntersect(allUserstoCheck[i], allUserstoCheck[j], intersect)
-
-for key in intersect:
-    if len(intersect[key]) > 1:
-        print(key, ": ", intersect[key])
+        for key in intersect:
+            if len(intersect[key]) > 1:
+                tweet += (
+                    str(index)
+                    + ") @"
+                    + str(key)
+                    + " x "
+                    + str(len(intersect[key]))
+                    + ":\n"
+                )
+                for i in range(len(intersect[key])):
+                    tweet += "@" + str(intersect[key][i])
+                    if i != len(intersect[key]) - 1:
+                        tweet += ", "
+                tweet += "\n\n"
+                index += 1
+        return tweet
